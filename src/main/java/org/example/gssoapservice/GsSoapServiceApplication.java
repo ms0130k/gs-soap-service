c433064bc5f7a04a138ad125c729d8e8123ae99b
KimDonghyeon <minseok0130@gmail.com>
2024-02-08 03:46:19 +0900

    init

---
+++ src/main/java/org/example/gssoapservice/GsSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.gssoapservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsSoapServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsSoapServiceApplication.class, args);
+    }
+
+}
